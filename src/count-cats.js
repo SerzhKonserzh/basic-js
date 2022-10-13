6a4f28f259923dbb5d4b82eebe430024f6ec27ae
SerzhKonserzh <Serzh-2001BTR@mail.ru>
2022-10-13 11:31:21 +0300

    feat: do cats and carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(cats) {
+  let count = 0;
+  cats.forEach(item=>{
+    item.forEach(elem=>{
+      if(elem=='^^'){
+        count++;
+      }
+    })
+  })
+  return count;
 }
 
 module.exports = {
